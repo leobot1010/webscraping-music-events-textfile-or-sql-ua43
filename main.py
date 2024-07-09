b7333454dd394b81d7d9b3d993648d5ef564cba7
Kieran <leobot1010@gmail.com>
2024-07-09 10:15:57 +0100

    Check automatically for new tours, send email if found.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import selectorlib
+import smtplib, ssl
+from email.message import EmailMessage
+import time
+
 
 URL = "https://programmer100.pythonanywhere.com/tours"
 HEADERS = {
@@ -19,8 +23,30 @@ def extract(source):
     value = extractor.extract(source)['tours']
     return value
 
-def send_email():
+def send_email(tour_info):
     """ Send email with details of new tour info """
+    host = "smtp.gmail.com"
+    port = 465
+
+    sender = "leobot1010@gmail.com"
+    password = 'vuihokpyyfphutnc'
+
+    receiver = 'kieran303@hotmail.com'
+    # receiver = 'leobot1010@gmail.com'
+
+    context = ssl.create_default_context()
+
+    em = EmailMessage()
+    em['From'] = sender
+    em['To'] = receiver
+    em['Subject'] = 'New Tour Announced'
+    em.set_content(tour_info)
+
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, em.as_string())
+
     print("Email was sent")
 
 
@@ -37,16 +63,18 @@ def read_data():
 
 
 if __name__ == '__main__':
-    scraped = scrape(URL)
-    extracted = extract(scraped)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
 
-    if extracted != "No upcoming tours":
-        stored_data = read_data()
-        print(stored_data)
-        if extracted not in stored_data:
-            store_data(extracted)
-            send_email()
-    print(extracted)
+        if extracted != "No upcoming tours":
+            stored_data = read_data()
+            print(stored_data)
+            if extracted not in stored_data:
+                store_data(extracted)
+                send_email(extracted)
+        time.sleep(5)
+        print(extracted)
 
 
 
